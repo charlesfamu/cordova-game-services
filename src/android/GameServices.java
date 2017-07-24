fa2c524ebf3574f3c7e57ccb3bc2da6e924b350c
Charles Bunton <charles.bunton@buntonco.com>
2017-07-24 00:00:36 -0400

    updates to games services

--- src/android/GameServices.java
+++ src/android/GameServices.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.auth.api.Auth;
@@ -25,7 +26,6 @@ import com.google.android.gms.games.Player;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.Games.GamesOptions;
 import com.google.android.gms.games.GamesActivityResultCodes;
-import com.google.example.games.basegameutils.BaseGameUtils;
 
 import java.security.MessageDigest;
 
@@ -42,12 +42,10 @@ public class GameServices extends CordovaPlugin implements
   GoogleApiClient.OnConnectionFailedListener {
 
     public static final String TAG = "GameServicesPlugin";
-    public static final int RC_GAMESERVICES = 4195819;
 
     private static final String ACTION_LOGIN = "login";
     private static final String ACTION_LOGOUT = "logout";
     private static final String ACTION_IS_SIGNEDIN = "isSignedIn";
-    private static final String ACTION_TOGGLE_DEBUG = "toggleDebugLog";
 
     private static final String ACTION_SUBMIT_SCORE = "submitScore";
     private static final String ACTION_SUBMIT_SCORE_NOW = "submitScoreNow";
@@ -61,24 +59,51 @@ public class GameServices extends CordovaPlugin implements
     private static final String ACTION_INCREMENT_ACHIEVEMENT_NOW = "incrementAchievementNow";
     private static final String ACTION_SHOW_ACHIEVEMENTS = "showAchievements";
     private static final String ACTION_SHOW_PLAYER = "showPlayer";
+    private static final int RC_SIGN_IN = 9001;
+    private static final int RC_GAMESERVICES = 4195819;
 
-    public static final String ARGUMENT_SCOPES = "scopes";
-    public static final String ARGUMENT_OFFLINE_KEY = "offline";
-
-    private GoogleApiClient mGoogleApiClient = null;
-    private CallbackContext savedCallbackContext = null;
-    Activity mActivity = null;
+    private GoogleApiClient mGoogleApiClient;
+    private CallbackContext savedCallbackContext;
+    Activity mActivity;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
       super.initialize(cordova, webView);
-
+      mActivity = cordova.getActivity();
     }
 
-	  @Override
+    @Override
   	public void onActivityResult(int request, int response, Intent intent) {
   		super.onActivityResult(request, response, intent);
   	}
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+      // if (this.mGoogleApiClient.hasConnectedApi(Games.API)) {
+      //   Auth.GoogleSignInApi.silentSignIn(this.mGoogleApiClient).setResultCallback(
+      //     new ResultCallback() {
+      //       @Override
+      //       public void onResult(GoogleSignInResult googleSignInResult) {
+      //         // In this case, we are sure the result is a success.
+      //         GoogleSignInAccount acct = googleSignInResult.getGoogleSignInAccount());
+      //          // Use the API client as normal.
+      //         Player player = Games.API.getCurrentPlayer(mApiClient);
+      //       }
+      //     }
+      //   );
+      // } else {
+      //   onSignedOut();
+      // }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Unresolvable failure in connecting to Google APIs");
+        this.savedCallbackContext.error(result.getErrorCode());
+    }
 
     @Override
     public void onStop(){
@@ -94,7 +119,7 @@ public class GameServices extends CordovaPlugin implements
           cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-              if (this.mGoogleApiClient == null) {
+              if (mGoogleApiClient == null) {
                 buildGoogleApliClient();
               }
               signIn();
@@ -133,24 +158,10 @@ public class GameServices extends CordovaPlugin implements
       return true;
     }
 
-    @Override
-    public void onSignInFailed() {
-      if (authCallbackContext != null) {
-        authCallbackContext.error("SIGN IN FAILED");
-      }
-    }
-
-    @Override
-    public void onSignInSucceeded() {
-      if (authCallbackContext != null) {
-        authCallbackContext.success("SIGN IN SUCCESS");
-      }
-    }
-
     private void signIn() {
       if (!this.mGoogleApiClient.isConnected()) {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(this.mGoogleApiClient);
-        cordova.getActivity().startActivityForResult(signInIntent, RC_GAMESERVICES);
+        cordova.getActivity().startActivityForResult(signInIntent, RC_SIGN_IN);
       }
       return;
     }
@@ -158,16 +169,16 @@ public class GameServices extends CordovaPlugin implements
     private void signOut() {
     }
 
-    private synchronized void buildGoogleApliClient() throws JSONException {
-      GoogleSignInOptions.Builder gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN);
+    private void buildGoogleApliClient() {
+      GoogleSignInOptions.Builder gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
       gso.requestEmail().requestProfile();
 
       Log.i(TAG, "Building GoogleApiClient");
       GoogleApiClient.Builder builder = new GoogleApiClient.Builder(webView.getContext())
         .addConnectionCallbacks(this)
         .addOnConnectionFailedListener(this)
-        .addApi(Auth.GOOGLE_SIGN_IN_API, gso.build())
-        .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+        .addApi(Games.API)
+        .addApi(Auth.GOOGLE_SIGN_IN_API, gso.build());
 
       this.mGoogleApiClient = builder.build();
 
